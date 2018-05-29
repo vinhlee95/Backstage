29e228fffd52483b6cc074feb608712143386411
Vinhlee95 <lethanhvinh95@gmail.com>
2018-05-29 13:58:31 +0300

    add redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, {}, compose( applyMiddleware(thunk)));
 
 ReactDOM.render(
-   <MuiThemeProvider><App /></MuiThemeProvider>, document.getElementById('root'));
+   <Provider store={store} >
+      <MuiThemeProvider><App /></MuiThemeProvider>
+   </Provider>
+   , document.getElementById('root'));
 registerServiceWorker();
